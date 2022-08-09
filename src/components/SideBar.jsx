615b23424e598c3a9e0ed5239fbb64ecaf981544
Nikitha <nikitharajhpk@gmail.com>
2022-08-09 11:44:45 +0530

    Added logout option

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,32 @@
 import "../styles/styles.css";
 import Icon from "../assets/List.png";
 import { useNavigate } from "react-router-dom";
+import { removeStorage } from "../services/Utils";
 
 const SideBar = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    removeStorage("idToken");
+    navigate(`/`);
+  };
 
-  const goToList=()=>{
-    navigate(`/list`)
-  }
+  const goToList = () => {
+    navigate(`/list`);
+  };
   return (
-    <aside class="aside">
-      <div class="tile">
-        <div class="imagebg">
+    <aside className="aside">
+      <div className="tile">
+        <div className="imagebg">
           <img src={Icon} />
         </div>
-        <a href=" " class="tile-text" onClick={()=>goToList()}>
-          
+        <a href=" " class="tile-text" onClick={() => goToList()}>
           Employee List
         </a>
+      </div>
+      <div className="logout">
+        <button className="button button1" onClick={() => goToLogin()}>
+          Logout
+        </button>
       </div>
     </aside>
   );
